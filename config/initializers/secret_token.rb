e7ca7353f4b454e9c30a8ddadd5fa365f648196e
Jeff Foster <jfoster@cs.umd.edu>
2011-12-24 16:55:31 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Talks::Application.config.secret_token = 'c29d970a6157c40514ad7a2c4bb2d69b34aa36df1937171a370c679993f18c3792fccfb19e0bcac024e664df295445ed344677b252b97081a6205472a2d3ec46'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Talks::Application.config.session_store :cookie_store, key: '_talks_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Talks::Application.config.session_store :active_record_store
