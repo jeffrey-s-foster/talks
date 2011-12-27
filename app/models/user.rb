2e0a380dfcb9aae38453b76c4202e28c8df91395
Jeff Foster <jfoster@cs.umd.edu>
2011-12-26 19:11:23 -0500

    Added missing changes from last commit; cleaned up devise form error messages a bit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :timeoutable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+
+  validates :name, :presence => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111226200401) do
+ActiveRecord::Schema.define(:version => 20111226235126) do
 
   create_table "talks", :force => true do |t|
     t.text     "title",      :default => ""
@@ -39,6 +39,9 @@ ActiveRecord::Schema.define(:version => 20111226200401) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.boolean  "perm_admin"
+    t.boolean  "perm_create_talk"
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
