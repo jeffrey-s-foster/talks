fb7464179dcfe34b909bc04c7d26b3f82cda0ad7
Jeff Foster <jfoster@cs.umd.edu>
2011-12-28 17:37:38 -0500

    clean up display of talks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
 
-  def join_with_and(a, m)
+  def join_with_and(a)
     return "" if a.length == 0
-    return a[0].send(m) if a.length == 1
-    return "#{a[0].send(m)} and #{a[1].send(m)}" if a.length == 2
-    return "#{a[0..-2].map { |x| x.send(m) }.join ","}, and #{a[-1].send(m)}"
+    return a[0] if a.length == 1
+    return "#{a[0]} and #{a[1]}" if a.length == 2
+    return "#{a[0..-2].join ","}, and #{a[-1]}"
   end
 end
