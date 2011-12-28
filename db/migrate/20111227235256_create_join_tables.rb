0910fe8ec7360b9d12ecd768852315c2b700ca81
Jeff Foster <jfoster@cs.umd.edu>
2011-12-27 19:11:14 -0500

    Fix up join tables again, add HABTM relationships

--- db/migrate/20111227235256_create_join_tables.rb
+++ db/migrate/20111227235256_create_join_tables.rb
@@ -13,21 +13,21 @@ class CreateJoinTables < ActiveRecord::Migration
     end
 
     create_table :lists_owners, :id => false do |t|
-      t.integer :list_id
+      t.integer :owned_list_id
       t.integer :owner_id
     end
 
     create_table :lists_posters, :id => false do |t|
-      t.integer :list_id
+      t.integer :poster_list_id
       t.integer :poster_id
     end
 
     add_index :lists_talks, :list_id
     add_index :lists_talks, :talk_id
     add_index :talks, :start_time
-    add_index :lists_owners, :list_id
+    add_index :lists_owners, :owned_list_id
     add_index :lists_owners, :owner_id
-    add_index :lists_posters, :list_id
+    add_index :lists_posters, :poster_list_id
     add_index :lists_posters, :poster_id
   end
 end
