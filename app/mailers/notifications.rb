24e7c9aa5770cfe5015a09073ab64aabe14e3410
Jeff Foster <jfoster@cs.umd.edu>
2012-01-26 16:39:27 -0500

    fixed mailing

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -2,10 +2,10 @@ class Notifications < ActionMailer::Base
   # time_interval can be :today or :this_week
   def send_talks(user, talks, subj)
     @talks = talks.sort { |a,b| a.start_time <=> b.start_time }
-    @subject = subj
+    @subj = subj
 
     mail :to => "#{user.name} <#{user.email}>",
-	:subject => "[talks] #{@subject}",
+	:subject => "[talks] #{@subj}",
 	:from => "Talks <jfoster@cs.umd.edu>"
   end
 end
