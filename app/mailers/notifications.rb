8a1cea621fb9e74a516b39ade8792738dbd31909
Jeff Foster <jfoster@cs.umd.edu>
2012-01-26 16:33:03 -0500

    more work on email

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -1,25 +1,11 @@
 class Notifications < ActionMailer::Base
   # time_interval can be :today or :this_week
-  def upcoming_talks(user, time_interval)
-    case time_interval
-    when :today
-      @talks = user.subscribed_talks(:today, [:kind_subscriber, :kind_subscriber_through]).keys
-	logger.debug "@talks = #{@talks.inspect}"
-      @subject = "Today's talks"
-    when :this_week
-      @talks = user.subscribed_talks(:this_week, [:kind_subscriber, :kind_subscriber_through]).keys
-      @subject = "This week's talks"
-    else
-      Rails.logger.error "Bad argument #{time_interval} to upcoming_talks"
-      return
-    end
+  def send_talks(user, talks, subj)
+    @talks = talks.sort { |a,b| a.start_time <=> b.start_time }
+    @subject = subj
 
-    return if @talks.empty?
-
-    @talks.sort! { |a,b| a.start_time <=> b.start_time }
-
-    mail :to => user.email,
+    mail :to => "#{user.name} <#{user.email}>",
 	:subject => "[talks] #{@subject}",
-	:from => "jfoster@cs.umd.edu"
+	:from => "Talks <jfoster@cs.umd.edu>"
   end
 end
--- lib/tasks/talks.rake
+++ lib/tasks/talks.rake
@@ -3,8 +3,13 @@ namespace :talks  do
   desc "Send email to subscribers of today's talks"
   task :today => :environment do
     Rails.logger.debug "Sending today's talks..."
-    u = User.find(1)
-    Notifications.upcoming_talks(u, :today).deliver
+    user = User.find(1)
+    talks = user.subscribed_talks(:today, [:kind_subscriber, :kind_subscriber_through]).keys
+    unless talks.empty?
+      Notifications.send_talks(user, talks, "Today's talks").deliver
+    else
+      Rails.logger.debug "Skipping #{user.email} - empty talks"
+    end
     Rails.logger.debug "Done sending"
   end
 
