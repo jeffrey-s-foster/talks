bed774c25b2279e23afa981f3e99dc930586c066
Jeff Foster <jfoster@cs.umd.edu>
2012-01-29 11:32:59 -0500

    fixed cron job/email task

--- cron-today.sh
+++ cron-today.sh
@@ -1,3 +1,3 @@
 #! /usr/local/bin/bash
 source /fs/bruce/jfoster_rr/.bash_profile
-rake -f /fs/bruce/jfoster_rr/talks/Rakefile talks:today
+rake -f /fs/bruce/jfoster_rr/talks/Rakefile talks:send_today
