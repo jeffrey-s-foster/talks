c9606e89f5022081f9dff05fc9411b7b7f061052
Jeff Foster <jfoster@cs.umd.edu>
2012-02-09 15:58:10 -0500

    moved speaker and venue rendering into helper methods

--- app/helpers/talks_helper.rb
+++ app/helpers/talks_helper.rb
@@ -1,2 +1,34 @@
 module TalksHelper
+
+  def render_speaker(talk)
+    if talk.speaker.empty?
+      @out = "No speaker yet"
+    else
+      @out = talk.speaker
+    end
+    unless talk.speaker_affiliation.empty?
+      @out += " - #{talk.speaker_affiliation}"
+    end
+    unless talk.speaker_url.empty?
+      @out = link_to @out, talk.speaker_url
+    end
+    return @out
+  end
+
+  def render_venue(talk)
+    return "<i>No venue yet</i>" unless (talk.room || talk.building)
+
+    if talk.building && (not talk.building.name.empty?)
+      @out = "#{talk.room} #{talk.building.name} (#{talk.building.abbrv})"
+    elsif talk.building
+      @out = "#{talk.room} #{talk.building.abbrv}"
+    else
+      @out = talk.room
+    end
+    if talk.building && talk.building.url && (not talk.building.url.empty?)
+      @out = link_to @out, talk.building.url
+    end
+    return @out
+  end
+
 end
