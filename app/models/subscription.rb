40c3261794ef1d7277727d6f35b4d340847f8c4f
Jeff Foster <jfoster@cs.umd.edu>
2015-04-02 14:10:45 -0400

    Switch another symbolize to enumerize, fix typo in test env.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,5 +2,5 @@ class Subscription < ActiveRecord::Base
   belongs_to :subscribable, :polymorphic => true
   belongs_to :user
 
-  symbolize :kind, :in => [:kind_watcher, :kind_subscriber], :scopes => true, :methods => true
+  enumerize :kind, in: [:kind_watcher, :kind_subscriber]
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,6 +36,8 @@ Talks::Application.configure do
   # like if you have constraints or database-specific column types
   # config.active_record.schema_format = :sql
 
+  config.active_support.test_order = :random
+  
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
 end
