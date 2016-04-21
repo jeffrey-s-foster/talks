fa023ad841e11585c61f861a61638315660823a0
Jeff Foster <jfoster@cs.umd.edu>
2016-04-21 19:40:06 -0400

    coverage in helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,6 @@
+require 'simplecov'
+SimpleCov.start
+
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
@@ -8,8 +11,6 @@ class ActiveSupport::TestCase
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
   fixtures :all
-
-  # Add more helper methods to be used by all tests here...
 end
 
 class ActionController::TestCase
