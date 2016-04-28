78e33bc3c2b0cfccefce52256ab4d93f20c1c96b
Jeff Foster <jfoster@cs.umd.edu>
2016-04-27 20:01:06 -0400

    A couple more create tests

--- test/controllers/talks_controller_test.rb
+++ test/controllers/talks_controller_test.rb
@@ -2,27 +2,8 @@ require 'test_helper'
 
 class TalksControllerTest < ActionController::TestCase
 
-  test "new not logged in" do
-    get :new
-    assert_redirected_to root_path
-  end
-
-  test "new hacker" do
-    sign_in users(:user_hacker)
-    get :new
-    assert_redirected_to root_path
-    sign_out users(:user_hacker)
-  end
-
-  test "new admin" do
-    sign_in users(:user_admin)
-    get :new
-    assert_response :success
-  end
-
-  test "create talk" do
-    sign_in users(:user_admin)
-    put :create, talk: {
+  def setup
+    @talk_hash = {talk: {
         title: "Test4298174",
         speaker: "Someone",
         speaker_affiliation: "Affiliation",
@@ -42,7 +23,41 @@ class TalksControllerTest < ActionController::TestCase
       temp_start_time: "",
       temp_end_time: "",
       posted_0: lists(:list_1),
-      posted_proto: ""
+      posted_proto: ""}
+  end
+
+  test "new not logged in" do
+    get :new
+    assert_redirected_to root_path
+  end
+
+  test "new hacker" do
+    sign_in users(:user_hacker)
+    get :new
+    assert_redirected_to root_path
+    sign_out users(:user_hacker)
+  end
+
+  test "new admin" do
+    sign_in users(:user_admin)
+    get :new
+    assert_response :success
+  end
+
+  test "create not logged in" do
+    put :create, @talk_hash
+    assert_redirected_to root_path
+  end
+
+  test "create hacker" do
+    sign_in users(:user_hacker)
+    put :create, @talk_hash
+    assert_redirected_to root_path
+  end
+
+  test "create admin" do
+    sign_in users(:user_admin)
+    put :create, @talk_hash
     t = Talk.find_by_title("Test4298174")
     assert_not_nil t
     assert_redirected_to talk_path(t)
