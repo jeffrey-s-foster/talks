2c42261f3e0ffcfc3c0b07d84e8da30b492f63a3
Jeff Foster <jfoster@cs.umd.edu>
2016-05-09 13:52:18 -0400

    trivial tests for admin controller

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class AdminControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index/spam hacker" do
+    sign_in users(:user_hacker)
+    get :index
+    assert_redirected_to root_path
+    get :spam
+    assert_redirected_to root_path
+  end
+
+  test "index/spam admin" do
+    sign_in users(:user_admin)
+    get :index
+    assert_response :success
+    get :spam
+    assert_response :success
+  end
 end
