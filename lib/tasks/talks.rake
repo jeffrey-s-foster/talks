741519c2d8b390196332ac89f1adae0847366f78
Jeff Foster <jfoster@cs.umd.edu>
2016-05-09 16:32:10 -0400

    Test rake tasks that send email

--- lib/tasks/talks.rake
+++ lib/tasks/talks.rake
@@ -5,7 +5,7 @@ namespace :talks  do
     Rails.logger.debug "Sending today's talks at #{Time.now}..."
     User.all.each do |user|
       next unless user.opt_email_today
-      talks = user.subscribed_talks(:today, [:kind_subscriber, :kind_subscriber_through]).keys
+      talks = user.subscribed_talks(:today, ["kind_subscriber", "kind_subscriber_through"]).keys
       unless talks.empty?
         TheMailer.send_talks(user, talks, "Today's talks").deliver_now
       else
@@ -20,7 +20,7 @@ namespace :talks  do
     Rails.logger.debug "Sending this weeky's talks at #{Time.now}..."
     User.all.each do |user|
       next unless user.opt_email_this_week
-      talks = user.subscribed_talks(:this_week, [:kind_subscriber, :kind_subscriber_through]).keys
+      talks = user.subscribed_talks(:this_week, ["kind_subscriber", "kind_subscriber_through"]).keys
       unless talks.empty?
         TheMailer.send_talks(user, talks, "This week's talks").deliver_now
       else
---
+++ test/lib/talks_task_test.rb
@@ -0,0 +1,34 @@
+class TalksTaskTest < ActionMailer::TestCase
+  # Inherits from ActionMailer::TestCase so mailings are reset
+
+  test "send today" do
+    u = users(:user_to_email)
+    travel_to Time.parse("2016-05-08 12:30:00 UTC") do
+      Rake::Task['talks:send_today'].invoke
+    end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    email = ActionMailer::Base.deliveries[0]
+    assert_equal [u.email], email.to
+    assert_equal ["talks@cs.umd.edu"], email.from
+    assert_match(/Subscribed Direct/, email.body.to_s)
+    assert_match(/Subscribed Indirect/, email.body.to_s)
+    assert_no_match(/Subscribed DTomorrow/, email.body.to_s)
+    assert_no_match(/Subscribed ITomorrow/, email.body.to_s)
+  end
+
+  test "send this week" do
+    u = users(:user_to_email)
+    travel_to Time.parse("2016-05-08 12:30:00 UTC") do
+      Rake::Task['talks:send_this_week'].invoke
+    end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    email = ActionMailer::Base.deliveries[0]
+    assert_equal [u.email], email.to
+    assert_equal ["talks@cs.umd.edu"], email.from
+    assert_match(/Subscribed Direct/, email.body.to_s)
+    assert_match(/Subscribed Indirect/, email.body.to_s)
+    assert_match(/Subscribed DTomorrow/, email.body.to_s)
+    assert_match(/Subscribed ITomorrow/, email.body.to_s)
+  end
+
+end
